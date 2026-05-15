d563a22bc4148740b546cbe190985d550615e1d5
Akshay-Onetab <akshaydeshmukh@onetab.ai>
2026-05-15 20:50:01 +0530

    changed the name to customer

--- customer/vite.config.js
+++ customer/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 
   server: {
     host: true,
-    port: 5175,
+    port: 5173,
     strictPort: true,
     allowedHosts: ["miniputkwink.com", "www.miniputkwink.com"]
   },
